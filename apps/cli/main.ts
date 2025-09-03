bdc3cfc61ba064406b84402ebe9c3d32ced36c85
theridwanade <theridwanade@gmail.com>
2025-09-03 14:36:31 +0100

    Implement interactive CLI for key pair generation and user input handling

--- apps/cli/main.ts
+++ apps/cli/main.ts
@@ -1,5 +1,21 @@
+import {bgBrightBlack} from "https://deno.land/std@0.224.0/fmt/colors.ts"
+import {input} from "@libs/cli-utils";
 import {generateKeyPair} from "@libs/crypto";
-
 (async function main () {
-    console.log((await generateKeyPair()).privateKeyPEM);
+    console.log(`Welcome to ${bgBrightBlack(" Anon-e2ee ")} CLI`);
+    while (true) {
+        console.log("1. Create a new key pair <Enter 1 to create new key pair>");
+        console.log("2. Exit <Enter 2 to exit>");
+        const choice = await input("> ");
+        if (choice === "1") {
+            console.log("Creating new key pair...");
+            console.log("Key pair created");
+            console.log(await generateKeyPair());
+        } else if (choice === "2") {
+            console.log("Exiting...");
+            break;
+        } else {
+            console.log("Invalid choice");
+        }
+    }
 })();
