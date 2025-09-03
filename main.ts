969e21ddfd9894b07d3c771f74121ba6d3539e21
theridwanade <theridwanade@gmail.com>
2025-09-03 10:36:02 +0100

    Create function to accept user input from the terminal

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-console.log("Hello, Deno!");
+const input = async (question: string): Promise<string> => {
+    await Deno.stdout.write(new TextEncoder().encode(question));
+    const buf = new Uint8Array(1024);
+    const n = <number>await Deno.stdin.read(buf);
+    return new TextDecoder().decode(buf.subarray(0, n)).trim();
+};
+
+async function main() {
+    console.log("Welcome to the Deno CLI application!");
+    const name = await input("What is your name? ");
+    console.log(`Hello, ${name}!`);
+};
+
+main();
