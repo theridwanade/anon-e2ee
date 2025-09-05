97500b6526f364f694d0bd0569bfd1a27f38ab9e
theridwanade <theridwanade@gmail.com>
2025-09-05 17:14:41 +0100

    Add message input handling and send functionality to `Chat` component
    - Introduced `useState` to manage message input state and facilitate user interaction.
    - Implemented `sendMessage` function to send user-entered messages via WebSocket.
    - Updated input field to synchronize with `message` state using `onChange`.
    - Added `onClick` handler to "Send" button for triggering `sendMessage`.

--- apps/anon-chat/islands/Chat.tsx
+++ apps/anon-chat/islands/Chat.tsx
@@ -1,4 +1,7 @@
+import { useState } from "preact/hooks";
+
 export default function Chat() {
+    const [message, setMessage] = useState("");
     const socket = new WebSocket("ws://localhost:8000/ws");
     socket.onopen = () => {
         console.log("WebSocket connection established");
@@ -6,11 +9,14 @@ export default function Chat() {
     socket.onmessage = (e) => {
         console.log("Message received from client", e.data);
     }
+    const sendMessage = () => {
+        socket.send(message);
+    }
     return  <div>
         <textarea id="log" cols="50" rows="10" readOnly className={"border"}></textarea><br/>
         <div className={"border p-2 flex"}>
-            <input id="msg" type="text" placeholder="Type a message..." className={"border p-2 m-2 w-[80%]"}/>
-            <button id="send" className={"border bg-slate-900 text-white px-4 py-2 text-xl rounded-xl"}>Send</button>
+            <input id="msg" type="text" placeholder="Type a message..." className={"border p-2 m-2 w-[80%]"} onChange={(e) => setMessage(e.currentTarget.value)} value={message}/>
+            <button id="send" className={"border bg-slate-900 text-white px-4 py-2 text-xl rounded-xl"} onClick={sendMessage}>Send</button>
         </div>
     </div>
 };
